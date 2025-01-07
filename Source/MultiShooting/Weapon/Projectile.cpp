88141d60a91dba23e23c9e8495cf9df868e706d9
ly <1397227739@qq.com>
2025-01-07 16:29:57 +0800

    添加发射物移动组件

--- Source/MultiShooting/Weapon/Projectile.cpp
+++ Source/MultiShooting/Weapon/Projectile.cpp
@@ -4,6 +4,7 @@
 #include "Projectile.h"
 #include "Components/SceneComponent.h"
 #include "Components/BoxComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 
 AProjectile::AProjectile()
@@ -21,6 +22,12 @@ AProjectile::AProjectile()
 	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
 	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic, ECollisionResponse::ECR_Block);
 
+	//发射物移动组件, 不需要附加根组件
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	//子弹的旋转跟随速度方向, 如应用重力时速度回向下呈抛物线, 弹头方向会每帧更新和速度保持一致
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+	ProjectileMovementComponent->InitialSpeed = 15000.f;
+	ProjectileMovementComponent->MaxSpeed = 15000.f;
 }
 
 void AProjectile::BeginPlay()
