79146318dd3c896a1afbca31301f4e0d1d851396
ly <1397227739@qq.com>
2025-01-08 17:25:11 +0800

    添加子弹壳音效和材质特效, 以及自动销毁

--- Source/MultiShooting/Weapon/Casing.cpp
+++ Source/MultiShooting/Weapon/Casing.cpp
@@ -1,8 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-//Todo: 弹壳音效和粒子
+// 不需要复制, 仅作为本地机器的视觉效果
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Engine/LatentActionManager.h"
 
 ACasing::ACasing()
 {
@@ -10,11 +14,49 @@ ACasing::ACasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+
+	//防止弹壳模型对SpringArm阻挡
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+
+	//启用物理模拟和重力
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+
+	//蓝图 : Simulation generates hit events
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+
+	//默认的冲量乘数
+	ShellEjectionImpulse = 5.f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
 	
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
 }
 
+void ACasing::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound && bFirstHit)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+
+	bFirstHit = false;
+
+	GetWorld()->GetTimerManager().SetTimer(DestroyTimeHandle, this, &ThisClass::DestroyFun, 3.f, false);
+}
+
+void ACasing::DestroyFun()
+{
+	Destroy();
+}
+
+void ACasing::Destroyed()
+{
+	Super::Destroyed();
+	GetWorldTimerManager().ClearTimer(DestroyTimeHandle);
+}
--- Source/MultiShooting/Weapon/Casing.h
+++ Source/MultiShooting/Weapon/Casing.h
@@ -13,12 +13,33 @@ class MULTISHOOTING_API ACasing : public AActor
 	
 public:	
 	ACasing();
+	virtual void Destroyed() override;
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(
+		UPrimitiveComponent* HitComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+	);
 
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
 
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
+
+	FTimerHandle DestroyTimeHandle;
+
+	void DestroyFun();
+
+	bool bFirstHit = true;		//只希望hit一次(播放一次音效)
 };
